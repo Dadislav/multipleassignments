8f3ecda5092762de313c408dbcff3ce2fad2a706
Dalibor <dadulinos@seznam.cz>
2021-11-13 15:19:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+a = b = c = d = []
+tmp = []
+# a = tmp
+# b = tmp
+# c = tmp
+# d = tmp
+
+print(a, b, c,d)
+print(a is b)
+
+x,y = x[:] = [[]], []
+# tmp = [[]], []
+# x, y = tmp
+# a[:] = tmp
+
+print(x,y)
+
+
+s,f = s[f] = s = [1,2,3], 2
+print(s,f)
